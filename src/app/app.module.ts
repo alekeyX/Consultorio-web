ac040ecebd518f59f2fd49f7eef3c6120d07ca46
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-03-22 23:21:00 -0400

    desarrollo de componentes home y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
