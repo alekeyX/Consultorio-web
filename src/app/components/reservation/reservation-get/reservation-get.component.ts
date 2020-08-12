7213ac45a7640c1fcc8fba3ae841d26ee3cb1f01
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-08-12 19:27:44 -0400

    Filtrado de reservas activas a partir de la fecha actual

--- src/app/components/reservation/reservation-get/reservation-get.component.ts
+++ src/app/components/reservation/reservation-get/reservation-get.component.ts
@@ -5,6 +5,7 @@ import { Role } from '../../models/role';
 import { ReservationService } from '../../services/reservation.service';
 import { MedicService } from '../../services/medic.service';
 import { Reservation } from '../../models/reservation';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-reservation-get',
@@ -14,11 +15,13 @@ import { Reservation } from '../../models/reservation';
 export class ReservationGetComponent implements OnInit {
   currentUser: any;
   reservations: Reservation[] = [];
+  reservationsActive: Reservation[] = [];
   filterDates: string[] = [];
   medics: any[] = [];
-  loading = false;
-  filterReservation = '';
-  filterReservationByMedic = '';
+  loading: boolean = false;
+  allReservations: boolean = false;
+  filterReservation: string = '';
+  filterReservationByMedic: string = '';
   order: string = '';
   asc: boolean = false;
 
@@ -33,22 +36,24 @@ export class ReservationGetComponent implements OnInit {
   ngOnInit(): void {
     this.currentUser = this.authenticationService.currentUserValue;
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    this.getReservas();    
+    this.getReservas();  
   }
-
+  
   getReservas() {
     setInterval(() => {this.loading = true; }, 800);
     if (this.currentUser.role === Role.Admin) {
       this.reservationService.getAll().subscribe((data) => {
-      this.reservations = data;
+        this.reservations = data;
       this.filterDate();
       this.getMedics();
+      this.ReservationsActually();
       });
     } else {
       if (this.currentUser.role === Role.Medic) {
         this.reservationService.getReservByMedic(this.currentUser._id).subscribe((data) => {
           this.reservations = data;
           this.filterDate();
+          this.ReservationsActually();
         })
       } else {
         if (this.currentUser.role == Role.Patient) {
@@ -56,6 +61,7 @@ export class ReservationGetComponent implements OnInit {
           .subscribe((data) => {
             this.reservations = data;
             this.filterDate();
+            this.ReservationsActually();
           })
         }
       }
@@ -80,6 +86,7 @@ export class ReservationGetComponent implements OnInit {
     this.router.navigate(['/reservation/', id]);
   }
 
+  // Eliminar duplicados del arreglo reservations
   filterDate() {
     const resultado = Array.from(new Set(this.reservations.map(element => element.date)))
     .map(date => {
@@ -91,6 +98,22 @@ export class ReservationGetComponent implements OnInit {
   orderBy(order: string) {
     this.asc = !this.asc;
     this.order = order;
+  }
+
+  // Arreglo de solo reservaciones a partir de la fecha actual
+  ReservationsActually() {
+    this.reservations.forEach(element => {
+      let year = parseInt(element.date.substring(0,4), 10),
+      month = parseInt(element.date.substring(5,7), 10),
+      day = parseInt(element.date.substring(8,10), 10);
+      
+      let date = new Date(year,month,day);
+      let today = new Date();
+
+      if (date > today) {
+        this.reservationsActive.push(element);
+      }
+    });
   }
 
   get isAdmin() {
