452d3c53f4c0042e8651ebd5940b3bc30c504f11
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2021-09-04 11:09:17 -0400

    mejora de editar historia clinica

--- src/app/components/history/history-edit/history-edit.component.ts
+++ src/app/components/history/history-edit/history-edit.component.ts
@@ -6,6 +6,10 @@ import { AuthenticationService } from '../../services/authentication.service';
 import { History } from '../../models/history';
 import { ToastrService } from 'ngx-toastr';
 import Swal from 'sweetalert2';
+import { Medic } from '../../models/medic';
+import { Patient } from '../../models/patient';
+import { ExamService } from '../../services/exam.service';
+import { DiagnosticService } from '../../services/diagnostic.service';
 
 @Component({
   selector: 'app-history-edit',
@@ -15,11 +19,13 @@ import Swal from 'sweetalert2';
 export class HistoryEditComponent implements OnInit {
 
   submitted = false;
-  patientId: any;
+  // patientId: any;
   angForm: FormGroup;
+  diagnosticForm: FormGroup;
+  examForm: FormGroup;
+  currentUser: Patient | Medic;
   history: History;
   historyData: History[];
-  currentUser: any;
   id: string;
   eFisico: boolean = false;
   ePiel: boolean = false;
@@ -34,10 +40,12 @@ export class HistoryEditComponent implements OnInit {
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    public historyService: HistoryService,
+    private historyService: HistoryService,
+    private examServ: ExamService,
+    private diagServ: DiagnosticService,
     private authenticationService: AuthenticationService,
     private toastr: ToastrService
-    ) {
+  ) {
   }
 
   ngOnInit() {
@@ -50,12 +58,13 @@ export class HistoryEditComponent implements OnInit {
 
   // Obetner datos de la historia clinica por id
   getHistory(id: string) {
-    this.historyService.getById(id).subscribe (data => {
-    this.patientId = data.patient_id;
-    this.angForm = this.fb.group({
-        medic: [data.medic],
+    this.historyService.getById(id).subscribe(data => {
+      // this.patientId = data.patient_id;
+      this.history = data;
+      this.angForm = this.fb.group({
+        medic: [data.medic, Validators.required],
         motivoConsulta: [data.motivoConsulta, Validators.required],
-        enfermedadActual: [data.enfermedadActual],
+        enfermedadActual: [data.enfermedadActual, Validators.required],
         antecedentesPersonales: [data.antecedentesPersonales],
         antecedentesFamiliares: [data.antecedentesFamiliares],
         age: [data.age],
@@ -72,50 +81,54 @@ export class HistoryEditComponent implements OnInit {
         facies: [data.facies],
         actitud: [data.actitud],
         decubito: [data.decubito],
-        marcha: [data.marcha],
+        marcha: [data.marcha]
+      });
+      this.examForm = this.fb.group({
         // Piel
-        // aspecto: [data.aspecto],
-        // distribucionPilosa: [data.distribucionPilosa],
-        // lesiones: [''],
-        // faneras: [data.faneras],
-        // tejidoCelularSub: [data.tejidoCelularSub],
-        // // Cabeza
-        // craneoCara: [data.craneoCara],
-        // cueroCabelludo: [data.cueroCabelludo],
-        // regionFrontal: [data.regionFrontal],
-        // regionOrbitonasal: [data.regionOrbitonasal],
-        // regionOrofaringea: [data.regionOrofaringea],
-        // //  Cuello
-        // cInspeccion: [data.cInspeccion],
-        // cPalpacion: [data.cPalpacion],
-        // cPercusion: [data.cPercusion],
-        // cAuscultacion: [data.cAuscultacion],
-        // // Respiratorio
-        // rInspeccion: [data.rInspeccion],
-        // rPalpacion: [data.rPalpacion],
-        // rPercusion: [data.rPercusion],
-        // rAuscultacion: [data.rAuscultacion],
-        // // Cardiovascular
-        // cdInspeccion: [data.cdInspeccion],
-        // cdPalpacion: [data.cdPalpacion],
-        // cdAuscultacion: [data.cdAuscultacion],
-        // cdPulsos: [data.cdPulsos],
-        // // Abdomen
-        // aInspeccion: [data.aInspeccion],
-        // aPalpacion: [data.aPalpacion],
-        // aPercusion: [data.aPercusion],
-        // aAuscultacion: [data.aAuscultacion],
-        // // Neurologo
-        // glasglow: [data.glasglow],
-        // motilidadActiva: [data.motilidadActiva],
-        // motilidadPasiva: [data.motilidadPasiva],
-        // motilidadRefleja: [data.motilidadRefleja],
-        // paresCraneales: [data.paresCraneales],
-        // sensibilidadProfunda: [data.sensibilidadProfunda],
-        // sensibilidadSuperficial: [data.sensibilidadSuperficial],
-        // // Diagnostico
-        // diagnostico: [data.diagnostic_id.diagnostico],
-        // tratamiento: [data.diagnostic_id.tratamiento],
+        aspecto: [data.exam_id.aspecto],
+        distribucionPilosa: [data.exam_id.distribucionPilosa],
+        lesiones: [''],
+        faneras: [data.exam_id.faneras],
+        tejidoCelularSub: [data.exam_id.tejidoCelularSub],
+        // Cabeza
+        craneoCara: [data.exam_id.craneoCara],
+        cueroCabelludo: [data.exam_id.cueroCabelludo],
+        regionFrontal: [data.exam_id.regionFrontal],
+        regionOrbitonasal: [data.exam_id.regionOrbitonasal],
+        regionOrofaringea: [data.exam_id.regionOrofaringea],
+        //  Cuello
+        cInspeccion: [data.exam_id.cInspeccion],
+        cPalpacion: [data.exam_id.cPalpacion],
+        cPercusion: [data.exam_id.cPercusion],
+        cAuscultacion: [data.exam_id.cAuscultacion],
+        // Respiratorio
+        rInspeccion: [data.exam_id.rInspeccion],
+        rPalpacion: [data.exam_id.rPalpacion],
+        rPercusion: [data.exam_id.rPercusion],
+        rAuscultacion: [data.exam_id.rAuscultacion],
+        // Cardiovascular
+        cdInspeccion: [data.exam_id.cdInspeccion],
+        cdPalpacion: [data.exam_id.cdPalpacion],
+        cdAuscultacion: [data.exam_id.cdAuscultacion],
+        cdPulsos: [data.exam_id.cdPulsos],
+        // Abdomen
+        aInspeccion: [data.exam_id.aInspeccion],
+        aPalpacion: [data.exam_id.aPalpacion],
+        aPercusion: [data.exam_id.aPercusion],
+        aAuscultacion: [data.exam_id.aAuscultacion],
+        // Neurologo
+        glasglow: [data.exam_id.glasglow],
+        motilidadActiva: [data.exam_id.motilidadActiva],
+        motilidadPasiva: [data.exam_id.motilidadPasiva],
+        motilidadRefleja: [data.exam_id.motilidadRefleja],
+        paresCraneales: [data.exam_id.paresCraneales],
+        sensibilidadProfunda: [data.exam_id.sensibilidadProfunda],
+        sensibilidadSuperficial: [data.exam_id.sensibilidadSuperficial]
+      });
+      this.diagnosticForm = this.fb.group({
+        // Diagnostico
+        diagnostico: [data.diagnostic_id.diagnostico],
+        tratamiento: [data.diagnostic_id.tratamiento]
       });
     });
   }
@@ -124,7 +137,6 @@ export class HistoryEditComponent implements OnInit {
   createForm() {
     this.angForm = this.fb.group({
       medic: [this.currentUser.firstName + ' ' + this.currentUser.lastName],
-      specialty: [this.currentUser.specialty],
       motivoConsulta: ['', Validators.required],
       enfermedadActual: [''],
       antecedentesPersonales: [''],
@@ -143,7 +155,9 @@ export class HistoryEditComponent implements OnInit {
       facies: [''],
       actitud: [''],
       decubito: [''],
-      marcha: [''],
+      marcha: ['']
+    });
+    this.examForm = this.fb.group({
       // Piel
       aspecto: [''],
       distribucionPilosa: [''],
@@ -183,11 +197,12 @@ export class HistoryEditComponent implements OnInit {
       motilidadRefleja: [''],
       paresCraneales: [''],
       sensibilidadProfunda: [''],
-      sensibilidadSuperficial: [''],
+      sensibilidadSuperficial: ['']
+    });
+    this.diagnosticForm = this.fb.group({
       // Diagnostico
       diagnostico: [''],
-      tratamiento: [''],
-      hour: [''],
+      tratamiento: ['']
     });
   }
 
@@ -207,16 +222,19 @@ export class HistoryEditComponent implements OnInit {
     }).then((result) => {
       this.submitted = true;
       if (!this.angForm.valid) {
-      return false;
+        return false;
       } else {
         if (result.isConfirmed) {
-          this.historyService.update(this.id, this.angForm.value)
-            .subscribe((res) => {
-              this.router.navigate(['/history', this.patientId._id]);
-              this.toastr.success(res.message, '');
-            }, (error) => {
-              this.toastr.error('Intente nuevamente', error);
+          this.examServ.update(this.history.exam_id._id, this.examForm.value).subscribe( () => {
+            this.diagServ.update(this.history.diagnostic_id._id, this.diagnosticForm.value).subscribe( () => {
+              this.historyService.update(this.id, this.angForm.value).subscribe((res) => {
+                this.router.navigate(['/history', this.history.patient_id._id]);
+                this.toastr.success(res.message, '');
+              }, (error) => {
+                this.toastr.error('Intente nuevamente', error);
+              });
             });
+          });
         }
       }
     });
